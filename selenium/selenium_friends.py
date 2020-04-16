717c62e73ffebcb1a66c6d7d688cc9eee1517219
username=GKow97 <gp.kowalczyk@student.uw.edu.pl>
2020-04-16 12:11:08 +0200

    Fixed list of friends to csv. Skip ad function appears if necessary. Some changes in time sleep and scroll.

--- selenium/selenium_friends.py
+++ selenium/selenium_friends.py
@@ -31,21 +31,20 @@ def login(driver):
     # login with filmweb account
     button = driver.find_element_by_xpath('//div[@class="authPage__button authButton authButton--filmweb"]')
     button.click()
-    time.sleep(2)
+    time.sleep(1)
     print("************************\nAfter choosing way of logging in\n*********\n")
 
     # provide credentials
     username_field = driver.find_element_by_xpath('//input[@name="j_username"]')
     my_email = input('Please provide your email:')
     username_field.send_keys(my_email)
-    time.sleep(2)
+    time.sleep(1)
 
     password_field = driver.find_element_by_xpath('//input[@name="j_password"]')
     my_pass = getpass.getpass('Please provide your password:')
+
     password_field.send_keys(my_pass)
-    time.sleep(2)
-    # if(driver.find_element_by_class_name("authPage__description authPage__description--error")):
-    #         print("Error in provided login or password!")
+    time.sleep(1)
 
     # accept the credentials
     button = driver.find_element_by_xpath(
@@ -58,31 +57,30 @@ def login(driver):
 def skip_ad(driver):
     button = driver.find_element_by_xpath('//button[@class="ws__skipButton"]')
     button.click()
-    time.sleep(3)
+    time.sleep(1)
 
     print("************************\nAfter ad:\n*********\n")
 
 
 def get_my_profile_url(driver):
     button = driver.find_element_by_id("userHeaderButton")
-    # button = driver.find_element_by_xpath('//span[@class="user__name-wrap"]')
     button.click()
-    time.sleep(3)
+    time.sleep(1)
     print("************************\nAfter clicking on user account\n*********\n")
+
     return driver.current_url
 
 
 def retrive_friends(driver, url):
     new_url = url + '/friends'
     driver.get(new_url)
-    time.sleep(1)
-    try:
-        button = driver.find_elements_by_xpath('//ul[@class="userBoxes__list"]/li')
-        friends_set = set(button)
-    except:
-        scroll(driver, 1)
-        button = driver.find_elements_by_xpath('//ul[@class="userBoxes__list"]/li')
-        friends_set = set(button)
+    time.sleep(0.7)
+
+    scroll(driver, 0.7)
+    time.sleep(0.7)
+    button = driver.find_elements_by_xpath('//ul[@class="userBoxes__list"]/li')
+    friends_set = set(button)
+
     return [friend.get_attribute("data-user-name") for friend in friends_set]
 
 
@@ -113,6 +111,7 @@ def scroll(driver, timeout):
 
 
 def dump_movies_to_csv(movies, path='friends.csv'):
+
     df = pd.DataFrame(movies)
 
     if os.path.exists(path):
@@ -128,52 +127,55 @@ def remove_csv(path='friends.csv'):
 
 def add_usernames(parameter, friends_set, driver):
     all_friends_set = set()
+    friend_friends = set()
+    ffriend_friends = set()
+
+    print("************************\nMy friends\n*********\n")
     for friend in friends_set:
+
         if (len(all_friends_set) < parameter):
             all_friends_set.add(friend)
             friend_url = get_profile_url(friend)
-            friend_friends = retrive_friends(driver, friend_url)
+            [friend_friends.add(f) for f in retrive_friends(driver, friend_url)]
         else:
             break
-    print("************************\nMy friends\n*********\n")
 
-    for guy in friend_friends:
+    print("************************\nMy friends' friends\n*********\n")
+    for guy in list(friend_friends):
         if (len(all_friends_set) < parameter):
             all_friends_set.add(guy)
             ffriend_url = get_profile_url(guy)
-            ffriend_friends = retrive_friends(driver, ffriend_url)
+            [ffriend_friends.add(f) for f in retrive_friends(driver, ffriend_url)]
         else:
             break
-    print("************************\nMy friends' friends\n*********\n")
 
-    for guys in ffriend_friends:
+    print("************************\nMy friends' friends of friends\n*********\n")
+    for guys in list(ffriend_friends):
         if (len(all_friends_set) < parameter):
             all_friends_set.add(guys)
         else:
             break
-    print("************************\nMy friends' friends of friends\n*********\n")
-
     return all_friends_set
 
 
 def main():
     url = 'http://www.filmweb.pl/login'
-    parameter = 100
+    parameter = 1000
 
     driver = connect(url)
     accept_RODO(driver)
     login(driver)
-    #    skip_ad(driver)
+    if driver.find_elements_by_class_name("ws__countdown"):
+        skip_ad(driver)
     profile_url = get_my_profile_url(driver)
 
-    friends_set = retrive_friends(driver, profile_url)
+    friends_set = set(retrive_friends(driver, profile_url))
     all_friends_set = add_usernames(parameter, friends_set, driver)
 
     remove_csv()
     dump_movies_to_csv(all_friends_set)
-    time.sleep(10)
+    time.sleep(3)
     driver.quit()
-
 
 if __name__ == "__main__":
     main()
