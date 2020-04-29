19c865921680e164d5cb9bf16e0c5a39a393dd45
GKow <62706686+GKow97@users.noreply.github.com>
2020-04-29 11:05:06 +0200

    Update Scrapy_friends_of_friends_of_friends.py

--- scrapy_friends/scrapy_friends/spiders/Scrapy_friends_of_friends_of_friends.py
+++ scrapy_friends/scrapy_friends/spiders/Scrapy_friends_of_friends_of_friends.py
@@ -1,7 +1,7 @@
 import scrapy
 
 # What we want to scrap (usernames of friends of friends of own friends)
-#Some extra information is commented, because it isn't necessary in case of this task
+# Some extra information is commented, because it isn't necessary in case of this task
 class Moviefff(scrapy.Item):
     username = scrapy.Field()
     # name = scrapy.Field()
@@ -25,7 +25,7 @@ class MovieSpider(scrapy.Spider):
         print("*****************************")
 
     # How to scrap usernames of third level friendship (friends of friends of friends)
-    #Function return dictionary in column username with usernames of friends in filmweb.pl page
+    # Function return dictionary in column username with usernames of friends in filmweb.pl page
     def parse(self, response):
         username_xpath = '//ul[@class="userBoxes__list"]//li//@data-user-name'
         # name_xpath = '//div[@class="userPreview userPreview__hasFullName"]//@data-f-name'
