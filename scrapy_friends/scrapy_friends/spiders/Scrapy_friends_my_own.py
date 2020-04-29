234fb7f980394f009fa296125a31f054d2772a84
GKow <62706686+GKow97@users.noreply.github.com>
2020-04-29 10:55:00 +0200

    Update Scrapy_friends_my_own.py
    Adding comments

--- scrapy_friends/scrapy_friends/spiders/Scrapy_friends_my_own.py
+++ scrapy_friends/scrapy_friends/spiders/Scrapy_friends_my_own.py
@@ -1,13 +1,17 @@
+#Importing library
 import scrapy
 
+#Defining class
 class Movief(scrapy.Item):
     friends_links = scrapy.Field()
 
+#Defining class (name to execute program + allowed domain and starting URL)
 class MovieSpider(scrapy.Spider):
     name = 'friends'
     allowed_domains = ['www.filmweb.pl']
     start_urls = ['http://www.filmweb.pl/user/Dr_Marvel/friends']
 
+    #getting friends of your own, links to their friends, which will be helpful in next level of friends (iteration)
     def parse(self, response):
         xpath_friends = '//ul[re:test(@class, "userBoxes__list")]//li//@data-user-name'
         selection = response.xpath(xpath_friends)
@@ -15,4 +19,3 @@ class MovieSpider(scrapy.Spider):
             l = Movief()
             l['friends_links'] = 'http://www.filmweb.pl/user/' + s.get() + '/friends'
             yield l
-#działa poprawnie
